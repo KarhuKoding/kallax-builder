730d19c3544c22353c8a1229d2a18bf88c925c54
JOPI-RTX3060\johan <johannes@walenta.dev>
2022-12-19 11:28:49 +0200

    added camer-controls, basic setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <Canvas>
+        
         <mesh>
           <boxBufferGeometry />
           <meshPhongMaterial />
         </mesh>
+
         <ambientLight args={[0xff0000]} intensity={0.1} />
-        <directionalLight position={[0, 0, 5]} intensity={0.5} />
+
+        <OrbitControls makeDefault />
       </Canvas>
     </div>
   );
