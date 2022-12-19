d373357b7123f7c8901942621a00e258ea956990
JOPI-RTX3060\johan <johannes@walenta.dev>
2022-12-19 11:23:11 +0200

    init from sourcetree

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import "./styles.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas>
+        <mesh>
+          <boxBufferGeometry />
+          <meshPhongMaterial />
+        </mesh>
+        <ambientLight args={[0xff0000]} intensity={0.1} />
+        <directionalLight position={[0, 0, 5]} intensity={0.5} />
+      </Canvas>
     </div>
   );
 }
-
-export default App;
