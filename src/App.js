00aee764a13de5e2637088af2105eade48469683
KarhuKoding <johannes@walenta.dev>
2023-01-02 17:32:14 +0200

    added RenderTarget Example for Step 4 to render closeup animation (screwdriver with Camlock, see Step4 )

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import { OrbitControls, Stage } from "@react-three/drei";
+import {
+  OrbitControls,
+  Stage,
+  Plane,
+  PerspectiveCamera,
+  TorusKnot,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import React, { Suspense } from "react";
+
+import React, { Suspense, useMemo, useRef } from "react";
+import { createPortal, useFrame } from "@react-three/fiber";
+import * as THREE from "three";
 
 import ScrollListener from "./lib/ScrollListener";
 // Components
@@ -9,6 +18,54 @@ import UI from "./components/UI/UI";
 
 import "./styles.css";
 
+function SpinningThing() {
+  const mesh = useRef();
+  useFrame(
+    () =>
+      (mesh.current.rotation.x =
+        mesh.current.rotation.y =
+        mesh.current.rotation.z +=
+          0.01)
+  );
+  return (
+    <TorusKnot ref={mesh} args={[1, 0.4, 100, 64]}>
+      <meshNormalMaterial attach="material" />
+    </TorusKnot>
+  );
+}
+
+function RenderTargetPlane() {
+  const cam = useRef();
+  const [scene, target] = useMemo(() => {
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color("orange");
+    const target = new THREE.WebGLMultisampleRenderTarget(1024, 1024, {
+      format: THREE.RGBFormat,
+      stencilBuffer: false,
+    });
+    target.samples = 8;
+    return [scene, target];
+  }, []);
+
+  useFrame((state) => {
+    cam.current.position.z =
+      5 + Math.sin(state.clock.getElapsedTime() * 1.5) * 2;
+    state.gl.setRenderTarget(target);
+    state.gl.render(scene, cam.current);
+    state.gl.setRenderTarget(null);
+  });
+
+  return (
+    <>
+      <PerspectiveCamera ref={cam} position={[0, 0, 3]} />
+      {createPortal(<SpinningThing />, scene)}
+      <Plane args={[2, 2, 2]}>
+        <meshStandardMaterial attach="material" map={target.texture} />
+      </Plane>
+    </>
+  );
+}
+
 export default function App() {
   return (
     <div className="App">
@@ -16,6 +73,8 @@ export default function App() {
         <Suspense fallback={null}>
           <Stage environment="city" intensity={0.6}>
             <ScrollListener>
+              <RenderTargetPlane />
+
               <Shelf />
 
               <UI />
