21e4268a81eb93d5d66d10086845b291c7a5ff30
KarhuKoding <johannes@walenta.dev>
2023-01-09 15:20:44 +0200

    redone sf11 animation (performance)

--- src/components/UI/Step11.js
+++ src/components/UI/Step11.js
@@ -1,32 +1,36 @@
 // Move Shelf up and Lift y to half of Shelt Height
 //Spinning Animation for the Shelf
-import Confetti from "react-confetti";
-import { useLayoutEffect } from "react";
-import { ninetyDeg } from "../../lib/constants";
-import { isInbetween, isOne, lerp } from "../../lib/helperfunctions";
-import { scrollStore } from "../../store/store";
+import { useScroll } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
+import Confetti from "react-confetti";
+import { ninetyDeg } from "../../lib/constants";
+import {
+  isInbetween,
+  isOne,
+  lerp,
+  roundNumber,
+} from "../../lib/helperfunctions";
+import { scrollStore } from "../../store/store";
 
 function Step11Animations({ shelf }) {
-  const { state } = scrollStore();
-  const sf11 = state.sf11;
-
-  const sf11Interpolated = lerp(0, ninetyDeg, sf11);
-  const sf11InterpolatedSecondary = lerp(0, 0.7, sf11);
-
+  const scroll = useScroll();
   //SpinningAnimation
   useFrame((state, delta) => {
     if (shelf.current === null) return;
-    else if (!isOne(sf11)) return (shelf.current.rotation.z = 0);
-    shelf.current.rotation.z += delta;
-  });
+    const sf11 = roundNumber(scroll.range(10 / 11, 1 / 11));
+    console.log(sf11, "sf11");
+    const sf11Interpolated = lerp(0, ninetyDeg, sf11);
+    const sf11InterpolatedSecondary = lerp(0, 0.7, sf11);
 
-  useLayoutEffect(() => {
-    // Rotation
-    if (shelf.current === null) return;
-    shelf.current.rotation.x = sf11Interpolated;
-    shelf.current.position.y = sf11InterpolatedSecondary;
-  }, [sf11, shelf, sf11Interpolated, sf11InterpolatedSecondary]);
+    if (isInbetween(sf11)) {
+      shelf.current.position.y = sf11InterpolatedSecondary;
+      shelf.current.rotation.x = sf11Interpolated;
+    } else if (isOne(sf11)) {
+      shelf.current.rotation.z += delta;
+    } else if (!isOne(sf11)) {
+      shelf.current.rotation.z = 0;
+    }
+  });
 
   return null;
 }
