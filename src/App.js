ea489b92b86bb979cef80ba1125b584a866a5233
KarhuKoding <johannes@walenta.dev>
2023-01-10 11:31:48 +0200

    removed pagemonitor due to performance issues, added performance monitor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { OrbitControls, Stage } from "@react-three/drei";
+import { OrbitControls, Stage, PerformanceMonitor } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import React, { Suspense } from "react";
+import React, { Suspense, useState } from "react";
 import { Stats } from "@react-three/drei";
 
 import Shelf from "./components/Shelf/Shelf";
@@ -10,10 +10,16 @@ import ScrollListener from "./lib/ScrollListener";
 import "./styles.css";
 
 export default function App() {
+  const [dpr, setDpr] = useState(1.5);
+
   return (
     <>
       <div className="App">
-        <Canvas shadows>
+        <Canvas gl={{ antialias: true }}>
+          <PerformanceMonitor
+            onIncline={() => setDpr(2)}
+            onDecline={() => setDpr(1)}
+          ></PerformanceMonitor>
           <Stats />
           <Suspense fallback={null}>
             <Stage environment="city" intensity={0.6} adjustCamera>
@@ -24,7 +30,6 @@ export default function App() {
           </Suspense>
           <OrbitControls enableZoom={false} makeDefault />
         </Canvas>
-
         <ScrollIcon />
       </div>
     </>
